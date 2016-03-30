59f62deb36e67f90e76e7afc75555ab6183abde4
Septi Robiatul Adawiyah <septirobiatuladawiyah@gmail.com>
2016-03-30 10:27:13 +0700

    Update 1b.java

--- 1b.java
+++ 1b.java
@@ -2,6 +2,7 @@ import javax.swing.JOptionPane;
 public class Fibonacci{
 	public static void main(String[]args){
 		int a = Integer.parseInt(JOptionPane.show.InputDialog("Input nilai awal deret Fibonacci : "));
+		int a = Integer.parseInt(JOptionPane.show.InputDialog("Input deret Fibonacci yang anda inginkan : "));
 	}
 	
 }
