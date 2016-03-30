2a7439a74584e3c28a660001b294517f1a9fc967
Septi Robiatul Adawiyah <septirobiatuladawiyah@gmail.com>
2016-03-30 10:19:31 +0700

    Added files via upload

---
+++ 1b.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class Fibonacci{
+	public static void main(String[]args){
+		int a = Integer.parseInt(JOptionPane.show.InputDialog("Input nilai awal deret Fibonacci : "));
+	}
+	
+}
